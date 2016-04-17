1e84379f2d4822e601278b10c86fd66150ab262e
lainnie <l.diallo.pro@gmail.com>
2016-04-17 15:32:01 +0000

    Add confirm alert on delete workday.

--- imports/client/containers/workday.jsx
+++ imports/client/containers/workday.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { $ } from 'meteor/jquery';
 import { moment } from 'meteor/momentjs:moment';
+import sweetalert from 'meteor/kevohagan:sweetalert';
 
 import { deleteWorkday } from '../actions';
 import { Workday } from '../components';
@@ -12,7 +13,14 @@ class WorkDayContainer extends React.Component {
       deleteWorkday: (evt) => {
         evt.preventDefault();
 
-        this.props.dispatch(deleteWorkday(this.props._id));
+        sweetAlert({
+          title: 'Are you sure ?',
+          type: 'warning',
+          closeOnConfirm: true,
+          showCancelButton: true
+        }, () => {
+          this.props.dispatch(deleteWorkday(this.props._id));
+        });
       }
     };
   }
